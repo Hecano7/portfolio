3430d3745c7b85d08cb89c7a4e6477b0fc7cc153
Hecan7 <7hecano@gmail.com>
2020-04-17 07:10:25 -0700

    make it better

---
+++ Public/assets/js/main.js
@@ -0,0 +1,24 @@
+var projects = document.getElementById("projects");
+
+projects.addEventListener("click", function(){
+	window.scrollTo({
+		top:1000,
+		bottom:0,
+		behavior: "smooth"
+	});
+});
+
+var email = document.getElementById("email");
+
+email.addEventListener("click", function(){
+	window.scrollTo({
+		top:2500,
+		bottom:0,
+		behavior: "smooth"
+	});
+});
+
+function sendEmail() {
+	document.getElementById("passthrough").click();
+};
+
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+const axios = require('axios');
+
+app.listen(8080, () => {
+    console.log("listening at http://localhost:8080")
+});
+app.use(express.static('Public'));
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.set('views' , './views');
+
+app.set('view engine' , 'ejs');
+
+app.get('/',(req, res) => {
+    res.sendFile('index.html', {root: __dirname })
+});
+
+app.get('/contact', (req, res) => {
+res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+res.render('thanks', { contact: req.body })
+});
+
+
