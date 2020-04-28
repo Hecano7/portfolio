1b3c40016f2a1f85d4e0e4ac29af8039396e5886
Hecan7 <7hecano@gmail.com>
2020-04-28 15:00:28 -0700

    setting environment variables on heroku

--- Public/assets/js/mailer.js
+++ Public/assets/js/mailer.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 var nodemailer = require("nodemailer");
 
 module.exports = (request) => {
-    console.log(process.env.EMAIL,process.env.PASSWORD,process.env.SENDTO);
+    // console.log(process.env.EMAIL,process.env.PASSWORD,process.env.SENDTO);
     // transporter
     const transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -19,7 +19,8 @@ module.exports = (request) => {
         text: `Message Body\n${request.body.message}`
     }
     // use the transporter to send email
-    transporter.sendMail(mailOptions, (err, res) => {console.log('mail sent')})
+    transporter.sendMail(mailOptions, (err, res) => {console.log(process.env.EMAIL,process.env.PASSWORD,process.env.SENDTO);
+    })
 
     return true;
 }
